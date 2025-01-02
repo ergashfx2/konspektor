b3ac49a296ad32058c9380791595903ec6b115c8
Abduvokhidov Ergashali <147835039+ergashfx2@users.noreply.github.com>
2025-01-03 04:23:00 +0500

    commit

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from loader import dp
 import middlewares, filters, handlers
 import logging
 logging.basicConfig(level=logging.INFO)
-from handlers.user.schedule_posting import DEFAULT_TZ
-print(DEFAULT_TZ.localize(datetime.datetime.now()))
+print(datetime.datetime.now())
 
 if __name__ == '__main__':
     executor.start_polling(dp)
